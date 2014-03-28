55155a105db6f53bf71c1982271d28f4dd60ba1b
Rex Morgan <rmorgan@uship.com>
2014-03-27 22:59:45 -0500

    Cleaning up usage.

--- examples/usage.js
+++ examples/usage.js
@@ -1,6 +1,6 @@
 // var uploadcare = require('uploadcare')('public_key', 'private_key');
-var uploadcare = require('../lib/main')('public_key', 'private_key'),
-    fs = require('fs');
+ var uploadcare = require('uploadcare')('public_key', 'private_key'),
+     fs = require('fs');
 
 function handler(error, response) {
   if(error) {
@@ -10,9 +10,13 @@ function handler(error, response) {
   }
 }
 
-uploadcare.files.info('file_id', handler);
-uploadcare.files.keep('file_id', handler);
-uploadcare.files.remove('file_id', handler);
+var path = './test-file.jpg';
+// Upload file
 uploadcare.file.upload(fs.createReadStream(path), function(err,res){
-    console.log(err,res);
+	// Store the file
+    uploadcare.files.store(res.file, handler);
+    // Info on file
+    uploadcare.files.info(res.file, handler);
+    // Remove the file
+    uploadcare.files.remove(res.file, handler);
 });
