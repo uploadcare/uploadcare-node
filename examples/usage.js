93eb0b43a785b0cfc25686750a93387087bb5b98
Rex Morgan <rex.morgan@gmail.com>
2015-06-15 12:37:49 -0500

    updating example.js with working fromUrl example.

--- examples/usage.js
+++ examples/usage.js
@@ -1,22 +1,36 @@
-// var uploadcare = require('uploadcare')('public_key', 'private_key');
- var uploadcare = require('uploadcare')('public_key', 'private_key'),
-     fs = require('fs');
+var uploadcare = require('uploadcare')('public_key', 'private_key'),
+    fs = require('fs');
 
-function handler(error, response) {
-  if(error) {
-    console.log('Error: ' + JSON.stringify(response));
-  } else {
-    console.log('Response: ' + JSON.stringify(response));
-  }
+function handler(method) {
+  return function(error, response) {
+    if(error) {
+      console.log('Error in ' + method + ': ' + JSON.stringify(response));
+    } else {
+      console.log('Response from ' + method + ': ' + JSON.stringify(response));
+    }
+  };
 }
 
 var path = './test-file.jpg';
 // Upload file
 uploadcare.file.upload(fs.createReadStream(path), function(err,res){
-	// Store the file
-    uploadcare.files.store(res.file, handler);
+    // The only thing returned when uploading is the file property.
+
+    // Store the file
+    uploadcare.files.store(res.file, handler('file.upload.store'));
     // Info on file
-    uploadcare.files.info(res.file, handler);
+    uploadcare.files.info(res.file, handler('file.upload.info'));
     // Remove the file
-    uploadcare.files.remove(res.file, handler);
+    uploadcare.files.remove(res.file, handler('file.upload.remove'));
+});
+
+uploadcare.file.fromUrl('http://i.imgur.com/L4lmrVu.jpg', function(err, res){
+    // file.fromUrl's response has file_id or uuid, it does not contain the "file" property like upload.
+
+    // Store the file
+    uploadcare.files.store(res.uuid, handler('file.fromUrl.store'));
+    // Info on file
+    uploadcare.files.info(res.uuid, handler('file.fromUrl.store'));
+    // Remove the file
+    uploadcare.files.remove(res.uuid, handler('file.fromUrl.store'));
 });
